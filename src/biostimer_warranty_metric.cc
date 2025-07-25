852bd133e3abd0316d955d65ecacade30e198c74
François-Régis Degott <FrancoisRegisDegott@eaton.com>
2025-07-25 14:41:37 +0200

    fix: produce computed end_warranty_date metric (outage metric compliant)
    Signed-off-by: François-Régis Degott <FrancoisRegisDegott@eaton.com>

--- src/biostimer_warranty_metric.cc
+++ src/biostimer_warranty_metric.cc
@@ -23,6 +23,7 @@
 
 #include <fty_common_db_dbpath.h>
 #include <fty_common_db_asset.h>
+#include <fty_proto.h>
 #include <fty_shm.h>
 #include <fty_log.h>
 
@@ -95,16 +96,35 @@ int main(int /*argc*/, char** /*argv*/)
         row["name"].get(name); // asset iname
 
         // write the "end_warranty_date" metric
-        const int ttl = 12 * 60 * 60; // 12 hours (sec)
-        int r = fty::shm::write_metric(name, keytag, std::to_string(day_diff), "day", ttl);
-        if (r == 0) {
-            log_info("%s: %s@%s = %d days (ttl: %d)",
-                AGENT_NAME, keytag.c_str(), name.c_str(), day_diff, ttl);
+        fty_proto_t* proto = fty_proto_new(FTY_PROTO_METRIC);
+        if (proto) {
+            // see systemd timer configuration (OnCalendar=)
+            const uint32_t ttl{12 * 60 * 60}; // 12 hours (sec)
+
+            fty_proto_set_name(proto, "%s", name.c_str()); // asset
+            fty_proto_set_type(proto, "%s", keytag.c_str());
+            fty_proto_set_value(proto, "%d", day_diff);
+            fty_proto_set_unit(proto, "%s", "day");
+            fty_proto_set_ttl(proto, ttl);
+
+            // mark the metric as 'computed' (see outage metric computatiion (fty-outage))
+            fty_proto_aux_insert(proto, "x-cm-count", "%d", 0);
+
+            int r = fty_shm_write_metric_proto(proto);
+            if (r == 0) {
+                log_info("%s: %s@%s = %d days (ttl: %d)",
+                    AGENT_NAME, keytag.c_str(), name.c_str(), day_diff, ttl);
+            }
+            else {
+                log_error("%s: write_metric '%s@%s' failed (r: %d)",
+                    AGENT_NAME, keytag.c_str(), name.c_str(), r);
+            }
         }
         else {
-            log_error("%s: write_metric '%s@%s' failed (r: %d)",
-                AGENT_NAME, keytag.c_str(), name.c_str(), r);
+            log_error("%s: proto_new %s@%s failed",
+                AGENT_NAME, keytag.c_str(), name.c_str());
         }
+        fty_proto_destroy(&proto);
     };
 
     ManageFtyLog::setInstanceFtylog(AGENT_NAME, FTY_COMMON_LOGGING_DEFAULT_CFG);
@@ -113,7 +133,7 @@ int main(int /*argc*/, char** /*argv*/)
 
     // first, remove end_warranty_date metrics from shm
     {
-        const int ttl{1}; // metric should be removed on the next reading
+        const int ttl{1}; // metric should be removed on the next reading (sec)
         fty::shm::shmMetrics results;
         fty::shm::read_metrics(".*", ".*end_warranty_date", results);
         for (const auto& it : results) {
